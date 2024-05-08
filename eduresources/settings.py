93a8478dd67bd00780f3c4144e7548cc87552dac
Caleb <calebpeter4@gmail.com>
2024-05-09 00:30:25 +0300

    db config

--- eduresources/settings.py
+++ eduresources/settings.py
@@ -92,13 +92,6 @@ elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
     }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 
 AUTH_PASSWORD_VALIDATORS = [
