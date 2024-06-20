dd9cfc2911a42609b0b9b1b5dd96ed0ca35d428b
Caleb <calebpeter4@gmail.com>
2024-06-20 03:53:17 +0300

    email change

--- eduresources/settings.py
+++ eduresources/settings.py
@@ -210,12 +210,14 @@ CELERY_BEAT_SCHEDULE = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'sm1.cloudoon.com'
+#EMAIL_HOST = 'sm1.cloudoon.com'
+EMAIL_HOST = 'mail.privateemail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = 'Mwalimu Focus <info@mwalimufocus.co.ke>'
+DEFAULT_FROM_EMAIL = 'Mwalimu Focus <info@mwalimufocus.com>'
+
 
 SITE_ID = 1
 
