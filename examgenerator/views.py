25b941629ed997ac6e6f044a12d4692a6dcb3e04
Caleb <calebpeter4@gmail.com>
2024-07-06 16:07:26 +0300

    exam gen form

--- examgenerator/views.py
+++ examgenerator/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 from .models import Topic, Question, Answer, SubQuestion, SubAnswer, SubSubQuestion, SubSubAnswer
+from django.core.exceptions import ObjectDoesNotExist
 
 
 
@@ -15,48 +16,81 @@ def get_topics(request):
 
 
 
-@csrf_exempt
+
+
 def save_question_data(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
-        
-        topic_id = data.get('topic_id')
-        question_text = data.get('question_text')
-        answer_text = data.get('answer_text')
-        sub_questions = data.get('sub_questions', [])
-        
         try:
-            topic = Topic.objects.get(id=topic_id)
-            question = Question.objects.create(topic=topic, question_text=question_text)
-            answer = Answer.objects.create(question=question, answer_text=answer_text)
-            
-            for sub_question_data in sub_questions:
-                sub_question_text = sub_question_data.get('sub_question_text')
-                sub_answer_text = sub_question_data.get('sub_answer_text')
-                sub_question = SubQuestion.objects.create(question=question, sub_question_text=sub_question_text)
-                sub_answer = SubAnswer.objects.create(sub_question=sub_question, sub_answer_text=sub_answer_text)
-                
-                sub_sub_questions = sub_question_data.get('sub_sub_questions', [])
-                for sub_sub_question_data in sub_sub_questions:
-                    sub_sub_question_text = sub_sub_question_data.get('sub_sub_question_text')
-                    sub_sub_answer_text = sub_sub_question_data.get('sub_sub_answer_text')
-                    sub_sub_question = SubSubQuestion.objects.create(sub_question=sub_question, sub_sub_question_text=sub_sub_question_text)
-                    sub_sub_answer = SubSubAnswer.objects.create(sub_sub_question=sub_sub_question, sub_sub_answer_text=sub_sub_answer_text)
-            
-            response_data = {
-                'status': 'success',
-                'message': 'Question and associated data saved successfully.'
-            }
-            
-            return JsonResponse(response_data)
-        
-        except Topic.DoesNotExist:
-            response_data = {
-                'status': 'error',
-                'message': 'Topic with ID {} does not exist.'.format(topic_id)
-            }
-            return JsonResponse(response_data, status=404)
-    
-    return JsonResponse({'status': 'error', 'message': 'Only POST requests are allowed.'}, status=405)
+            topic_id = request.POST.get('topic_id')
+            if not topic_id:
+                return JsonResponse({'status': 'error', 'message': 'Topic ID not provided'})
+
+            topic = Topic.objects.get(pk=topic_id)
+            question_text = request.POST.get('question_text')
+            question_image = request.FILES.get('question_image')
+
+            question = Question.objects.create(
+                topic=topic,
+                question_text=question_text,
+                image=question_image
+            )
+
+            answer_text = request.POST.get('answer_text')
+            answer_image = request.FILES.get('answer_image')
+            if answer_text or answer_image:
+                Answer.objects.create(
+                    question=question,
+                    answer_text=answer_text,
+                    image=answer_image
+                )
+
+            sub_question_ids = {key.split('[')[1].split(']')[0] for key in request.POST if key.startswith('sub_questions[')}
+            for sub_question_id in sub_question_ids:
+                sub_question_text = request.POST.get(f'sub_questions[{sub_question_id}][sub_question_text]')
+                sub_question_image = request.FILES.get(f'sub_questions[{sub_question_id}][sub_question_image]')
+                sub_answer_text = request.POST.get(f'sub_questions[{sub_question_id}][sub_answer_text]')
+                sub_answer_image = request.FILES.get(f'sub_questions[{sub_question_id}][sub_answer_image]')
+
+                sub_question = SubQuestion.objects.create(
+                    question=question,
+                    sub_question_text=sub_question_text,
+                    image=sub_question_image
+                )
+
+                if sub_answer_text or sub_answer_image:
+                    SubAnswer.objects.create(
+                        sub_question=sub_question,
+                        sub_answer_text=sub_answer_text,
+                        image=sub_answer_image
+                    )
+
+                sub_sub_question_ids = {key.split('[')[3].split(']')[0] for key in request.POST if key.startswith(f'sub_questions[{sub_question_id}][sub_sub_questions][')}
+                for sub_sub_question_id in sub_sub_question_ids:
+                    sub_sub_question_text = request.POST.get(f'sub_questions[{sub_question_id}][sub_sub_questions][{sub_sub_question_id}][sub_sub_question_text]')
+                    sub_sub_question_image = request.FILES.get(f'sub_questions[{sub_question_id}][sub_sub_questions][{sub_sub_question_id}][sub_sub_question_image]')
+                    sub_sub_answer_text = request.POST.get(f'sub_questions[{sub_question_id}][sub_sub_questions][{sub_sub_question_id}][sub_sub_answer_text]')
+                    sub_sub_answer_image = request.FILES.get(f'sub_questions[{sub_question_id}][sub_sub_questions][{sub_sub_question_id}][sub_sub_answer_image]')
+
+                    sub_sub_question = SubSubQuestion.objects.create(
+                        sub_question=sub_question,
+                        sub_sub_question_text=sub_sub_question_text,
+                        image=sub_sub_question_image
+                    )
+
+                    if sub_sub_answer_text or sub_sub_answer_image:
+                        SubSubAnswer.objects.create(
+                            sub_sub_question=sub_sub_question,
+                            sub_sub_answer_text=sub_sub_answer_text,
+                            image=sub_sub_answer_image
+                        )
+
+            return JsonResponse({'status': 'success'})
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)})
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
+
+
+
+
 
 
