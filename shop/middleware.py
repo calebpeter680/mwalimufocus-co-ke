a1ae4acf68c5b83dfdc499c9497f9118701627ba
Caleb <calebpeter680@gmail.com>
2024-12-16 04:15:40 +0300

    fix cls

--- shop/middleware.py
+++ shop/middleware.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect
 from django.utils.deprecation import MiddlewareMixin
 import re
+import logging
 
 class DomainRedirectMiddleware:
     def __init__(self, get_response):
@@ -14,13 +15,14 @@ class DomainRedirectMiddleware:
         return response
 
 
-
-
-
 class RemoveCloudflareScriptMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         if response.status_code == 200:
             content = response.content.decode('utf-8')
             content = re.sub(r'<script.*src=["\'][^"\']*cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js.*<\/script>', '', content)
             response.content = content.encode('utf-8')
+
+            logging.debug("Response Content After Script Removal: %s", content)
+
         return response
+
